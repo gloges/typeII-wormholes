2b22bea32ca754c06f79af9df8db90e13bfc78f1
gloges <gerg1995@gmail.com>
2022-11-06 01:31:53 +0900

    Reverted to f from w & flat-space limit

--- wormholes.py
+++ wormholes.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 
 # TODO Flesh out docstrings
 # TODO Check optional argument default values
+# TODO Remove w
 
 
 def Q(r, q0):
@@ -21,11 +22,11 @@ def Qdd(r, q0):
     """Returns second derivative of q=sqrt(q0^2 + r^2)."""
     return q0**2 / Q(r, q0)**3
 
-def w_event(r, y, q0, flux):
-    """Stops integration if w -> 0."""
-    w = y[0]
-    return w - 0.1
-w_event.terminal = True
+def f_event(r, y, q0, flux):
+    """Stops integration if f > 1/q (appropriate for AdS)."""
+    f = y[0]
+    return f - 3/Q(r, q0)
+f_event.terminal = True
 
 
 ###################################################################################################
@@ -47,10 +48,10 @@ def dVdφ_S3S3(u, φ):
     return -(5/2)*np.exp(-9*u-φ/2) + (5/2)*np.exp(-3*u+5*φ/2)
 
 def ODEs_S3S3(r, y, q0, f4sqr):
-    """ODEs for w, u, φ and h."""
+    """ODEs for f, u, φ and h."""
 
     # Unpack
-    w, u, ud, φ, φd, h = y
+    f, u, ud, φ, φd, h = y
 
     q = Q(r, q0)
     qd = Qd(r, q0)
@@ -61,34 +62,37 @@ def ODEs_S3S3(r, y, q0, f4sqr):
     φ_d = φd
     
     # Equations of motion
-    prefactor = qdd/qd - qd/q - 1/(2*q*qd*w) * (4/q**2 - V_S3S3(u, φ))
+    prefactor = qdd/qd - qd/q - f**2 / (q*qd) * (2 - 1/2 * q**2 * V_S3S3(u, φ))
     
-    w_d  = -2*w*(prefactor + 4*qd/q)
-    ud_d = prefactor*ud + 1/(q**2 * w) * ((1/12)*dVdu_S3S3(u, φ) + f4sqr*np.exp(3*u-φ/2)/(8*q**6))
-    φd_d = prefactor*φd + 1/(q**2 * w) * (       dVdφ_S3S3(u, φ) - f4sqr*np.exp(3*u-φ/2)/(4*q**6))
-    h_d  = 1/(q**4 * np.sqrt(abs(w)))
+    f_d  = (prefactor + 3*qd/q) * f
+    ud_d = prefactor*ud + f**2 * ((1/12)*dVdu_S3S3(u, φ) + f4sqr*np.exp(3*u-φ/2)/(8*q**6))
+    φd_d = prefactor*φd + f**2 * (       dVdφ_S3S3(u, φ) - f4sqr*np.exp(3*u-φ/2)/(4*q**6))
+    h_d  = f/q**3
     
-    # Return derivatives of w, u, ud, φ, φd and h
-    return w_d, u_d, ud_d, φ_d, φd_d, h_d
+    # Return derivatives of f, u, ud, φ, φd and h
+    return f_d, u_d, ud_d, φ_d, φd_d, h_d
 
 def solve_S3S3(q0, rmax, u0, φ0, rmin=10**-8, nr=1000):
     """Solves ODEs for S3xS3 out to r=rmax for given q0 and initial conditions u0,φ0."""
 
-    # Initial condition for w0 and the value of f4^2 are set by u0 and φ0
-    w0 = 2/q0**2 - V_S3S3(u0, φ0)/2
-    f4sqr = 4*np.exp(-3*u0+φ0/2) * q0**4 * (1 + q0**2 * w0)
+    # Initial condition for f0 and the value of f4^2 are set by u0 and φ0
+    # f0m2 = f0**(-2)
+    f0m2 = 2 - 1/2 * q0**2 * V_S3S3(u0, φ0)
+    f4sqr = 4*np.exp(-3*u0+φ0/2) * q0**4 * (1 + f0m2)
     
-    if w0 < 0:
-        # Regular solutions must have w0>0
-        return [w0]
+    if f0m2 < 0:
+        # Regular solutions must have f0^2 > 0
+        return [f0m2]
+
+    f0 = f0m2**(-1/2)
 
     # Use series solutions (e.g. u = u0 + 1/2*udd0 * r^2 + ...)
     # to get initial conditions at r=rmin, where 0 < rmin << q0
-    udd0 = 1/(q0**2 * w0) * ( 3/(2*q0**2) - (1/4)*V_S3S3(u0, φ0) + (1/12)*dVdu_S3S3(u0, φ0))
-    φdd0 = 1/(q0**2 * w0) * (-3/q0**2     + (1/2)*V_S3S3(u0, φ0) +        dVdφ_S3S3(u0, φ0))
-    wdd0 = -6/q0**4 + V_S3S3(u0, φ0)/q0**2 - (1/4)*udd0*dVdu_S3S3(u0, φ0) - (1/4)*φdd0*dVdφ_S3S3(u0, φ0)
-    
-    w_start = w0 + (1/2)*wdd0*rmin**2
+    udd0 = f0**2 * ( 3/(2*q0**2) - (1/4)*V_S3S3(u0, φ0) + (1/12)*dVdu_S3S3(u0, φ0))
+    φdd0 = f0**2 * (-3/q0**2     + (1/2)*V_S3S3(u0, φ0) +        dVdφ_S3S3(u0, φ0))
+    fdd0 = (f0**3 * q0**2 / 8) * (8/q0**4 + udd0*dVdu_S3S3(u0, φ0) + φdd0*dVdφ_S3S3(u0, φ0))
+
+    f_start = f0 + (1/2)*fdd0*rmin**2
 
     u_start = u0 + (1/2)*udd0*rmin**2
     ud_start = udd0 * rmin
@@ -96,22 +100,23 @@ def solve_S3S3(q0, rmax, u0, φ0, rmin=10**-8, nr=1000):
     φ_start = φ0 + (1/2)*φdd0*rmin**2
     φd_start = φdd0 * rmin
 
-    h_start = rmin / (q0**4 * np.sqrt(w0))
+    h_start = rmin * f0 / q0**3
 
     # Initial conditions and constants
-    y0 = (w_start, u_start, ud_start, φ_start, φd_start, h_start)
+    y0 = (f_start, u_start, ud_start, φ_start, φd_start, h_start)
     args = (q0, f4sqr)
 
     soln = solve_ivp(ODEs_S3S3, (rmin, rmax),
                      y0=y0,
                      args=args,
-                     events=(w_event),
-                     t_eval=np.linspace(rmin, rmax, nr),
+                     events=(f_event),
+                    #  t_eval=np.linspace(rmin, rmax, nr),
+                     t_eval=np.geomspace(rmin, rmax, nr),
                      rtol=10**-8,
                      method='RK45'
                     )
 
-    # Return (r, w, u, ud, φ, φd, h, f4sqr)
+    # Return (r, f, u, ud, φ, φd, h, f4sqr)
     return [soln.t, *soln.y, f4sqr]
 
 def objective_S3S3(uφ0, q0, rmax, rmin=10**-8, nr=1000, display_progress=False):
@@ -120,14 +125,14 @@ def objective_S3S3(uφ0, q0, rmax, rmin=10**-8, nr=1000, display_progress=False)
     soln = solve_S3S3(q0, rmax, *uφ0, rmin)
 
     if len(soln) == 1:
-        # Invlid (w0<0): return (const.)+|w0| to drive towards w0>0
+        # Invalid (f0**2<0): return (const.)+|f0| to drive towards f0**2>0
         if display_progress:
             print('{:24.20f} {:24.20f}\t invalid'.format(*uφ0))
 
         value = 10**8 + abs(soln[0])
     else:
         # Unpack solution
-        r, w, u, ud, φ, φd, h, f4sqr = soln
+        r, f, u, ud, φ, φd, h, f4sqr = soln
         
         Δ1 = (3/2) + np.sqrt((3/2)**2 + 6)      # Conformal dimension for light mode
 
@@ -198,7 +203,7 @@ def wormhole_S3S3(q0, rmax, rmax_steps=3, uφ0=[0,0], xatol=10**-8, nr=1000, dis
         if ii+1 == rmax_steps:
             xatol_ii = xatol
 
-        print('  rmax = {:.4f} with xatol = {}'.format(rmax_ii, xatol_ii))
+        print('    rmax = {:.4f} with xatol = {}'.format(rmax_ii, xatol_ii))
 
         # Shooting method: determine u0,φ0 to match boundary conditions/scaling solutions at r>>q0
         opt = minimize(lambda uφ0: objective_S3S3(uφ0, q0, rmax_ii, display_progress=display_progress),
@@ -211,26 +216,25 @@ def wormhole_S3S3(q0, rmax, rmax_steps=3, uφ0=[0,0], xatol=10**-8, nr=1000, dis
         uφ0_best = opt.x
 
         # Print results
-        print('{:>10} : {}'.format('f_eval', opt.nfev))
-        print('{:>10} : {:+.10g}'.format('u0', uφ0_best[0]))
-        print('{:>10} : {:+.10g}'.format('φ0', uφ0_best[1]))
-        print('{:>10} : {:.10g}'.format('val', opt.fun))
+        print('{:>14} : {}'.format('f_eval', opt.nfev))
+        print('{:>14} : {:+.10g}'.format('u0', uφ0_best[0]))
+        print('{:>14} : {:+.10g}'.format('φ0', uφ0_best[1]))
+        print('{:>14} : {:.10g}'.format('val', opt.fun))
 
     # Get numerical solution for optimial initial conditions
     soln = solve_S3S3(q0, rmax, *uφ0_best, nr=nr)
-    r, w, u, ud, φ, φd, h, f4sqr = soln
 
     return symmetrize_S3S3(soln)
 
 def symmetrize_S3S3(soln):
     """Extend solutions from 0 < r < rmax to -rmax < r < rmax."""
 
-    r, w, u, ud, φ, φd, h, f4sqr = soln
+    r, f, u, ud, φ, φd, h, f4sqr = soln
     r[0] = 0
 
     # Make functions even/odd about r=0
     r = np.append(-r[-1:0:-1], [r])
-    w = np.append( w[-1:0:-1], [w])
+    f = np.append( f[-1:0:-1], [f])
     u = np.append( u[-1:0:-1], [u])
     φ = np.append( φ[-1:0:-1], [φ])
     h = np.append(-h[-1:0:-1], [h])
@@ -238,15 +242,14 @@ def symmetrize_S3S3(soln):
     ud = np.append(-ud[-1:0:-1], [ud])
     φd = np.append(-φd[-1:0:-1], [φd])
 
-    return r, w, u, ud, φ, φd, h, f4sqr
+    return r, f, u, ud, φ, φd, h, f4sqr
 
 def massless_approx_S3S3(q0):
     
     # Taking h(0) = 0, first compute h(inf)
-    h_inf = quad(lambda x: q0**-2 * x**2 * (q0**2 + x**2 - (1+q0**2)*x**6)**(-1/2),
-                 0, 1,
-                 args=(q0)
-                )
+    h_inf, err = quad(lambda x: q0**-2 * x**2 * (q0**2 + x**2 - (1+q0**2)*x**6)**(-1/2),
+                      0, 1
+                     )
 
     # φ = -2u has profile exp(φ) = cos(1/2 * sqrt(-c)*h) / cos(1/2 * sqrt(-c)*hinf),
     #   so φ(0) = -log[cos(1/2 * sqrt(-c)*hinf)]
@@ -277,10 +280,10 @@ def dVdv_T11(u, v):
     return (16/3)*np.exp(-8/3*(4*u+v)) * (np.exp(4*u+4*v) + 3*np.exp(6*u+2*v) - 4)
 
 def ODEs_T11(r, y, q0, f3sqr):
-    """ODEs for w, u, v, φ and h."""
+    """ODEs for f, u, v, φ and h."""
 
     # Unpack
-    w, u, ud, v, vd, φ, φd, h = y
+    f, u, ud, v, vd, φ, φd, h = y
 
     q = Q(r, q0)
     qd = Qd(r, q0)
@@ -292,37 +295,40 @@ def ODEs_T11(r, y, q0, f3sqr):
     φ_d = φd
 
     # Equations of motion
-    prefactor = qdd/qd - qd/q - 1/(3*q*qd*w) * (9/q**2 - V_T11(u, v))
+    prefactor = qdd/qd - qd/q - f**2/(q*qd) * (3 - 1/3 * q**2 * V_T11(u, v))
     
-    w_d  = -2*w*(prefactor + 5*qd/q)
-    ud_d = prefactor*ud + 1/(16*q**2 * w) * ( dVdu_T11(u, v) -   dVdv_T11(u, v) + 2*f3sqr*np.exp(4*u-φ)/q**8)
-    vd_d = prefactor*vd + 1/(16*q**2 * w) * (-dVdu_T11(u, v) + 7*dVdv_T11(u, v) - 2*f3sqr*np.exp(4*u-φ)/q**8)
-    φd_d = prefactor*φd - f3sqr*np.exp(4*u-φ)/(2*q**10 * w)
+    f_d  = (prefactor + 4*qd/q) * f
+    ud_d = prefactor*ud + (f**2 / 16) * ( dVdu_T11(u, v) -   dVdv_T11(u, v) + 2*f3sqr*np.exp(4*u-φ)/q**8)
+    vd_d = prefactor*vd + (f**2 / 16) * (-dVdu_T11(u, v) + 7*dVdv_T11(u, v) - 2*f3sqr*np.exp(4*u-φ)/q**8)
+    φd_d = prefactor*φd - f3sqr*np.exp(4*u-φ) * f**2/(2*q**8)
 
-    h_d = 1/(q**5 * np.sqrt(abs(w)))
+    h_d = f/q**4
 
     # Return derivatives of w, u, ud, v, vd, φ, φd and h
-    return w_d, u_d, ud_d, v_d, vd_d, φ_d, φd_d, h_d
+    return f_d, u_d, ud_d, v_d, vd_d, φ_d, φd_d, h_d
 
 def solve_T11(q0, rmax, u0, v0, φ0, rmin=10**-8, nr=1000):
     """Solves ODEs for T11 out to r=rmax for given q0 and initial conditions u0,v0,φ0."""
 
-    # Initial condition for w0 and the value of f3^2 are set by u0, v0 and φ0
-    w0 = 3/q0**2 - V_T11(u0, v0)/3
-    f3sqr = 6*np.exp(-4*u0+φ0) * q0**6 * (1 + q0**2 * w0)
+    # Initial condition for f0 and the value of f3^2 are set by u0, v0 and φ0
+    # f0m2 = f0**(-2)
+    f0m2 = 3 - 1/3 * q0**2 * V_T11(u0, v0)
+    f3sqr = 6*np.exp(-4*u0+φ0) * q0**6 * (1 + f0m2)
     
-    if w0 < 0:
-        # Regular solutions must have w0>0
-        return [w0]
+    if f0m2 < 0:
+        # Regular solutions must have f0**2>0
+        return [f0m2]
+
+    f0 = f0m2**(-1/2)
 
     # Use series solutions (e.g. u = u0 + 1/2*udd0 * r^2 + ...)
     # to get initial conditions at 0 < rmin << q0
-    udd0 = 1/(16*q0**2 * w0) * ( dVdu_T11(u0, v0) -   dVdv_T11(u0, v0) + 2*f3sqr*np.exp(4*u0-φ0)/q0**8)
-    vdd0 = 1/(16*q0**2 * w0) * (-dVdu_T11(u0, v0) + 7*dVdv_T11(u0, v0) - 2*f3sqr*np.exp(4*u0-φ0)/q0**8)
-    φdd0 = -f3sqr*np.exp(4*u0-φ0)/(2*q0**10 * w0)
-    wdd0 = -12/q0**4 + V_T11(u0, v0)/q0**2 - (1/6)*udd0*dVdu_T11(u0, v0) - (1/6)*vdd0*dVdv_T11(u0, v0)
+    udd0 = (f0**2 / 16) * ( dVdu_T11(u0, v0) -   dVdv_T11(u0, v0) + 2*f3sqr*np.exp(4*u0-φ0)/q0**8)
+    vdd0 = (f0**2 / 16) * (-dVdu_T11(u0, v0) + 7*dVdv_T11(u0, v0) - 2*f3sqr*np.exp(4*u0-φ0)/q0**8)
+    φdd0 = -f3sqr*np.exp(4*u0-φ0) * f0**2/(2*q0**8)
+    fdd0 = (f0**3 * q0**2 / 12) * (6*(3+f0**(-2))/q0**4 + udd0*dVdu_T11(u0, v0) + vdd0*dVdv_T11(u0, v0))
     
-    w_start = w0 + (1/2)*wdd0*rmin**2
+    f_start = f0 + (1/2)*fdd0*rmin**2
 
     u_start = u0 + (1/2)*udd0*rmin**2
     ud_start = udd0 * rmin
@@ -333,22 +339,22 @@ def solve_T11(q0, rmax, u0, v0, φ0, rmin=10**-8, nr=1000):
     φ_start = φ0 + (1/2)*φdd0*rmin**2
     φd_start = φdd0 * rmin
 
-    h_start = rmin / (q0**5 * np.sqrt(w0))
+    h_start = rmin * f0 / q0**4
 
     # Initial conditions and constants
-    y0 = (w_start, u_start, ud_start, v_start, vd_start, φ_start, φd_start, h_start)
+    y0 = (f_start, u_start, ud_start, v_start, vd_start, φ_start, φd_start, h_start)
     args = (q0, f3sqr)
 
     soln = solve_ivp(ODEs_T11, (rmin, rmax),
                      y0=y0,
                      args=args,
-                     events=(w_event),
-                     t_eval=np.linspace(rmin, rmax, nr),
+                     events=(f_event),
+                     t_eval=np.geomspace(rmin, rmax, nr),
                      rtol=10**-8,
                      method='RK45'
                     )
 
-    # Return (r, w, u, ud, v, vd, φ, φd, h, f4sqr)
+    # Return (r, f, u, ud, v, vd, φ, φd, h, f4sqr)
     return [soln.t, *soln.y, f3sqr]
 
 def objective_T11(uv0, φ0, q0, rmax, rmin=10**-8, nr=1000, display_progress=False):
@@ -357,14 +363,14 @@ def objective_T11(uv0, φ0, q0, rmax, rmin=10**-8, nr=1000, display_progress=Fal
     soln = solve_T11(q0, rmax, *uv0, φ0, rmin)
 
     if len(soln) == 1:
-        # Invlid (w0<0): return (const.)+|w0| to drive towards w0>0
+        # Invlid (f0**2<0): return (const.)+|f0| to drive towards f0**2>0
         if display_progress:
             print('{:24.20f} {:24.20f}\tinvalid'.format(*uv0))
 
         value = 10**8 + abs(soln[0])
     else:
         # Unpack solution
-        r, w, u, ud, v, vd, φ, φd, h, f3sqr = soln
+        r, f, u, ud, v, vd, φ, φd, h, f3sqr = soln
 
         Δ1 = (4/2) + np.sqrt((4/2)**2 + 12)     # Conformal dimension for light mode
 
@@ -436,7 +442,7 @@ def wormhole_T11(q0, rmax, rmax_steps=3, uv0=[0,0], xatol=10**-8, nr=1000, displ
         if ii+1 == rmax_steps:
             xatol_ii = xatol
 
-        print('  rmax = {:.4f} with xatol = {}'.format(rmax_ii, xatol_ii))
+        print('    rmax = {:.4f} with xatol = {}'.format(rmax_ii, xatol_ii))
 
         # Shooting method: determine u0,v0 to match boundary conditions/scaling solutions at r>>q0
         # Set φ0 = 0 for optimizing u0,v0 since can always absorb constant into f3^2 at the end
@@ -450,14 +456,14 @@ def wormhole_T11(q0, rmax, rmax_steps=3, uv0=[0,0], xatol=10**-8, nr=1000, displ
         uv0_best = opt.x
 
         # Print results
-        print('{:>10} : {}'.format('f_eval', opt.nfev))
-        print('{:>10} : {:+.10g}'.format('u0', uv0_best[0]))
-        print('{:>10} : {:+.10g}'.format('v0', uv0_best[1]))
-        print('{:>10} : {:.10g}'.format('val', opt.fun))
+        print('{:>14} : {}'.format('f_eval', opt.nfev))
+        print('{:>14} : {:+.10g}'.format('u0', uv0_best[0]))
+        print('{:>14} : {:+.10g}'.format('v0', uv0_best[1]))
+        print('{:>14} : {:.10g}'.format('val', opt.fun))
 
     # Get numerical solution for optimial initial conditions
     soln = solve_T11(q0, rmax, *uv0_best, 0, nr=nr)
-    r, w, u, ud, v, vd, φ, φd, h, f3sqr = soln
+    r, f, u, ud, v, vd, φ, φd, h, f3sqr = soln
 
     # Shift φ and rescale flux so that φ(inf) -> 0
     # This leverages the fact that only the combination f^2 * exp(-φ) appears in the equations of motion
@@ -470,12 +476,12 @@ def wormhole_T11(q0, rmax, rmax_steps=3, uv0=[0,0], xatol=10**-8, nr=1000, displ
 def symmetrize_T11(soln):
     """Extend solutions from 0 < r < rmax to -rmax < r < rmax."""
 
-    r, w, u, ud, v, vd, φ, φd, h, f3sqr = soln
+    r, f, u, ud, v, vd, φ, φd, h, f3sqr = soln
     r[0] = 0
 
     # Make (anti-)symmetric about r=0
     r = np.append(-r[-1:0:-1], [r])
-    w = np.append( w[-1:0:-1], [w])
+    f = np.append( f[-1:0:-1], [f])
     u = np.append( u[-1:0:-1], [u])
     v = np.append( v[-1:0:-1], [v])
     φ = np.append( φ[-1:0:-1], [φ])
@@ -485,7 +491,7 @@ def symmetrize_T11(soln):
     vd = np.append(-vd[-1:0:-1], [vd])
     φd = np.append(-φd[-1:0:-1], [φd])
 
-    return r, w, u, ud, v, vd, φ, φd, h, f3sqr
+    return r, f, u, ud, v, vd, φ, φd, h, f3sqr
 
 def massless_approx_T11(q0):
     
